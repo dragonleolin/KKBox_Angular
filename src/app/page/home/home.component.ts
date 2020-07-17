3935854823cb278be1c950dbfb7f006ac9f0434d
dragonleolin <42775462+dragonleolin@users.noreply.github.com>
2020-07-17 17:59:18 +0800

    已經取到值，但ngFor還不能成功

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -8,40 +8,58 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-
-  constructor(private testService:TestService, private http: HttpClient){
-  }
+  constructor(private testService: TestService, private http: HttpClient) {}
   ngOnInit(): void {
     // this.getData();
-    // console.log('123', this.getData());s
+    // console.log('123', this.getData());
     this.getNewHitPlayLists();
+
+    console.log('test:', this.datas);
+    console.log('test2:', this.Heros);
   }
 
   // getData(){
   //   return this.testService.getNewHitPlayLists();
   // }
-datas=[];
-   //取得每周熱門歌曲排行封面
-   getNewHitPlayLists = async () => {
+
+  datas: any[] = [];
+
+  Heros = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' },
+  ];
+
+  //取得每周熱門歌曲排行封面
+  getNewHitPlayLists = () => {
     const token = this.testService.token;
     // console.log(1, token);
-    await this.http
+    this.http
       .get('https://api.kkbox.com/v1.1/new-hits-playlists?territory=TW', {
         headers: {
-          Authorization: `Bearer ` + token
+          Authorization: `Bearer ` + token,
         },
       })
       .subscribe({
-        next(value){
-          this.datas = value;
-          console.log('HoemValue',value);
-          console.log(' this.datas', this.datas);
+        next(value) {
+          this.datas = [value];
+          this.datas = this.datas[0].data;
+          console.log('HoemValue', typeof value);
+          console.log(' this.datas', this.datas[0].data);
+          console.log(' this.datas1', this.datas);
+        },error(error){
+          console.log('error',error);
         },
+        complete(){
+          console.log('success',this.datas);
+        }
       });
-      
-  };
-
-
-
+  }
 }
